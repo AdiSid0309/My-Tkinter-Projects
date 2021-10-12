b8d26557565fce7cadcef67a7131f3359f6e05a8
AdiSid0309 <50744962+AdiSid0309@users.noreply.github.com>
2021-10-12 13:39:42 +0530

    Add files via upload
    commited

--- Face_Detection/Face_Detection.py
+++ Face_Detection/Face_Detection.py
@@ -1,7 +1,4 @@
-import numpy
-from pygame import mixer
 import cv2
-import time
 from tkinter import *
 import tkinter.messagebox
 from PIL import Image, ImageTk
@@ -9,44 +6,38 @@ from PIL import Image, ImageTk
 #-------------------------------Main Frame Creation-------------------------
 
 root = Tk()
-root.geometry('500x600')
+root.geometry('500x550')
 frame = Frame(root, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH, expand=1)
 root.title('Face Detection')
-photo = PhotoImage(file="User_icon.png")
+photo = PhotoImage(file="ui.png")
 root.iconphoto(False, photo)
-frame.config(background='light blue')
-label = Label(frame, text="Face Detection", bg='light blue', font=('Times 35 bold'))
+frame.config(background='black')
+label = Label(frame, text="Face Detection", fg="white", bg='black', font=('Algerian 35 bold'))
 label.pack(side=TOP)
-filename = PhotoImage(file="demo.png")
+filename = PhotoImage(file="ai.png")
 background_label = Label(frame, image=filename)
 background_label.pack(side=TOP)
 
 #-------------------------------Tools and About Menu-------------------------
 
-def hel():
-    help(cv2)
-
 def Contri():
-   tkinter.messagebox.showinfo("Contributors", "\n1.Siddharth Soni\n2. Jatin Mishra \n3. Kunal Chahar \n")
+   tkinter.messagebox.showinfo("Programmers", "\n1.Siddharth Soni\n2. Jatin Mishra \n3. Kunal Chahar \n")
 
 
 def anotherWin():
-   tkinter.messagebox.showinfo("About", 'Driver Cam version v1.0\n Made Using\n-OpenCV\n-Numpy\n-Tkinter\n In Python 3')
+   tkinter.messagebox.showinfo("About", 'Driver Cam version-Using ivCam\n Made Using\n-OpenCV\n-Tkinter\n-Python 3')
 
 
 
 menu = Menu(root)
 root.config(menu=menu)
 
-subm1 = Menu(menu)
-menu.add_cascade(label="Tools", menu=subm1)
-subm1.add_command(label="Open CV Docs", command=hel)
 
-subm2 = Menu(menu)
-menu.add_cascade(label="About", menu=subm2)
-subm2.add_command(label="Driver Cam", command=anotherWin)
-subm2.add_command(label="Contributors", command=Contri)
+subm1 = Menu(menu)
+menu.add_cascade(label="About", menu=subm1)
+subm1.add_command(label="Details", command=anotherWin)
+subm1.add_command(label="Programmers", command=Contri)
 
 #-------------------------------Main Functions For Buttons-------------------------
 
@@ -133,51 +124,6 @@ def webdetRec():
    cv2.destroyAllWindows()
 
 
-'''def alert():
-   mixer.init()
-   alert=mixer.Sound('beep-07.wav')
-   alert.play()
-   time.sleep(0.1)
-   alert.play()
-
-def blink():
-   capture = cv2.VideoCapture(0)
-   face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
-   eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-   blink_cascade = cv2.CascadeClassifier('CustomBlinkCascade.xml')
-
-   while True:
-      ret, frame = capture.read()
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      faces = face_cascade.detectMultiScale(gray)
-
-      for (x, y, w, h) in faces:
-         font = cv2.FONT_HERSHEY_COMPLEX
-         cv2.putText(frame, 'Face', (x+w, y+h), font, 1, (250, 250, 250), 2, cv2.LINE_AA)
-         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-         roi_gray = gray[y:y+h, x:x+w]
-         roi_color = frame[y:y+h, x:x+w]
-
-         eyes = eye_cascade.detectMultiScale(roi_gray)
-
-         for(ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
-         blink = blink_cascade.detectMultiScale(roi_gray)
-
-         for (eyx, eyy, eyw, eyh) in blink:
-            cv2.rectangle(roi_color, (eyx, eyy), (eyx+eyw, eyy+eyh), (255, 255, 0), 2)
-            alert()
-
-      cv2.imshow('frame',frame)
-
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
-
-
-   capture.release()
-   cv2.destroyAllWindows()
-'''
 #-------------------------------Placement Of Buttons-------------------------
 photo0 = Image.open("op_cam.png")
 photo0 = photo0.resize((70, 180), Image.ANTIALIAS)
@@ -194,7 +140,7 @@ photo2 = ImageTk.PhotoImage(photo2)
 photo = PhotoImage(file = "exit1.png")
 
 
-but0=Button(frame, padx=5, pady=5, bg='white', fg='black', relief=GROOVE, command=web, image=photo0, height=70, width=100)
+but0 = Button(frame, padx=5, pady=5, bg='white', fg='black', relief=GROOVE, command=web, image=photo0, height=70, width=100)
 but0.place(x=200,y=104)
 
 but1 = Button(frame, padx=5, pady=5, bg='white', fg='black', relief=GROOVE, command=webdet, image=photo1, height=100, width=100)
@@ -206,5 +152,7 @@ but2.place(x=200, y=320)
 but3 = Button(frame, padx=5, pady=5, bg='white', fg='black', relief=GROOVE, command=exitt, image=photo, height=70, width=180)
 but3.place(x=160, y=440)
 
+
+root.resizable(False, False)
 
 root.mainloop()
